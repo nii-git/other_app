87815659648b1ba5af9fa52ae3a23fb956e650de
nii-git <murasa.nui1@gmail.com>
2023-07-01 17:00:22 +0900

    webscraping_仮完成

--- English-frequency/webscraping.py
+++ English-frequency/webscraping.py
@@ -1,17 +1,43 @@
 from bs4 import BeautifulSoup
-import requests,re
+import requests,datetime
 
-def GetArticleUrl():
-    None
+
+
+class Media:
+    ID: int
+    Name: str
+    RSSUrl: str
+    SelectRule: str
+
+    def __init__(self,i,n,r,s):
+        self.ID = i
+        self.Name = n
+        self.RSSUrl = r
+        self.SelectRule = s
+
+    
+MEDIALIST = []
+MEDIALIST.append(Media(1,"BSS","http://feeds.bbci.co.uk/news/rss.xml#",'body > div > div > div > div > div > div > div > main > article > div > div > p[class*="Paragraph"]'))
+
+def GetArticleUrlFromRSS(rss_url):
+    resultLinks = []
+    req = requests.get(rss_url)
+    selectRule = 'link'
+    if req.status_code != 200:
+        print(f"GetArticleUrlFromRSS ERROR: Request Failed. Status Code is {req.status_code}")
+        return resultLinks
+    
+    soup = BeautifulSoup(req.text,"xml")
+    links = soup.select(selectRule)
+
+    for l in links:
+        resultLinks.append(l.get_text())
+
+    return resultLinks
 
 
 def GetArticle(url,rule):
-    resultArticle = []
-    if rule == "BBS":
-        selectRule = 'body > div > div > div > div > div > div > div > main > article > div > div > p[class*="Paragraph"]'
-    else:
-        print("GetArticle ERROR: Rule is invalid")
-        return resultArticle
+    resultArticles = []
     
     req = requests.get(url)
 
@@ -20,14 +46,32 @@ def GetArticle(url,rule):
 
 
     soup = BeautifulSoup(req.text,"html.parser")
-    article = soup.select(selectRule)
+    article = soup.select(rule)
 
     for a in article:
-        resultArticle.append(a.get_text())
-        # print(a.get_text())
+        resultArticles.append(a.get_text())
 
-    return resultArticle
+    return resultArticles
+
+
+def OutputStringListToFile(article_list,file_name):
+    try:
+        with open('./output/'+file_name,mode='w') as f:
+            for l in article_list:
+                f.write(l)
+        return 0
+    except Exception as e:
+        print(f"OutputStringListToFile ERROR: {e}")
+        return -1
 
 if __name__ == '__main__':
-    article = GetArticle("https://www.bbc.com/news/world-europe-66070033?at_medium=RSS&at_campaign=KARANGA","BBS")
-    print(article[0])
+    media = MEDIALIST[0]
+    urls = GetArticleUrlFromRSS(media.RSSUrl)
+    fileNum = 1
+    for url in urls:
+        articleList = GetArticle(url,media.SelectRule)
+        filename = media.Name + datetime.datetime.now().strftime('%Y-%m-%d') + "-" + str(fileNum)
+        result = OutputStringListToFile(articleList,filename)
+        if result != 0:
+            break
+        fileNum += 1
