05d153cd3593a55df5f8e43f9505f656e35ce49e
nii-git <murasa.nui1@gmail.com>
2023-09-13 10:27:50 +0900

    local用頻度出力機能実装

--- English-frequency/local_src/storedata.py
+++ English-frequency/local_src/storedata.py
@@ -1,4 +1,5 @@
 import glob,sys,re
+import pandas as pd
 
 def local_init():
     if len(sys.argv) != 3:
@@ -13,6 +14,14 @@ def local_getpath(path):
     files = glob.glob(path)
     return files
 
+def local_output(df_groupby,provider,day):
+    with open("./result/" + provider + "/" + day + "/result.csv","a") as f:
+        for i in range(0,df_groupby.count()):
+            index = df_groupby.index[i]
+            values = df_groupby.values[i]
+            f.write(index + "," + str(values) + "\n")
+
+
 
 
 if __name__ == "__main__":
@@ -21,16 +30,24 @@ if __name__ == "__main__":
     targetPaths = local_getpath("./output/" + provider + "/" + day + "/*")
     print(targetPaths)
 
-    # 英字分割パターン
-    pattern = re.compile(r'.|,|\s|\t')
+    articles = []
 
     for t in targetPaths:
         try: 
             print(t)
             with open(t,"r") as f:
                 #エラー出るならサイズを分ける
-                articleList = re.split(r'[.,\s]',f.read(-1))
+                articleList = re.split(r'\.|\,|\s|\t',f.read(-1))
         except Exception as e:
             print("[ERROR]FileOpenError: " + str(e))
         
-        break
+        articles += [re.sub('\"|\!|\?',"",a.lower()) for a in articleList]
+    
+    df = pd.DataFrame(articles,columns=["word"])
+
+    df_groupby = df.groupby(["word"]).size()
+
+    local_output(df_groupby,provider,day)
+
+    # print(df_groupby.count())
+    
